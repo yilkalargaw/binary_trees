c038ab9b21a278aa50b5fa59d60ea51113428197
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-01 23:19:26 +0000

    attempting 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,9 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 0;
 
 	if (tree != NULL)
-	{
-		size++;
-		size += binary_tree_size(tree->left) + binary_tree_size(tree->right);
-	}
+		size += 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
 	return (size);
 }
